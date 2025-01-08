8fc7d4edd8ef24a4dd3bc6197cfc33ea30e2b3a2
fzhnf <faizahnaf52@gmail.com>
2025-01-08 14:17:43 +0800

    style: change color for better readability

--- lib/core/theme/pallete.dart
+++ lib/core/theme/pallete.dart
@@ -5,7 +5,7 @@ class AppPallete {
   static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
   static const Color gradient3 = Color.fromRGBO(255, 159, 124, 1);
-  static const Color gradient4 = Color.fromRGBO(255, 218, 121, 1);
+  static const Color gradient4 = Color.fromRGBO(102, 51, 153, 1);
   static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
